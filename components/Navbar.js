7d20a1f561c30354e292a991e4b421618f2ae6f1
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-28 20:00:22 +0530

    added logo but do some sttyling gfor the large scren devices

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,38 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
-import "../styles/Navbar.css";
+import '../components/Navbar.css'
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <Link href="/">Home</Link>
-      <Link href="/cars">Cars</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <div className="navbar-container">
+        {/* LOGO on the left */}
+        <div className="logo">
+          
+        </div>
+
+        {/* Hamburger on the right (for small devices) */}
+        <div
+          className={`menu-icon ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation Links */}
+        <div className={`nav-links ${menuOpen ? "show" : ""}`}>
+          <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link href="/cars" onClick={() => setMenuOpen(false)}>Cars</Link>
+          <Link href="/about" onClick={() => setMenuOpen(false)}>About</Link>
+          <Link href="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+        </div>
+      </div>
     </nav>
   );
 }
