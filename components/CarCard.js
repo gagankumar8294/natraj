765ca2d1773d333cfe0b93d9bd8ca8bc2112bdc8
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-30 09:28:43 +0530

    whatsapp redirectng with defaut chat text is added

--- components/CarCard.js
+++ components/CarCard.js
@@ -1,6 +1,17 @@
+"use client";
 import '../components/CarCard.css'
 
 export default function CarCard({ name, price, image }) {
+
+  const phoneNumber = "9164143196";
+  const rawMessage = `Hi *Nataraj Tours & Travels*\ni want to book cab - *${name}*\nCall back please`;
+  const encodedMessage = encodeURIComponent(rawMessage);
+
+  const handleWhatsAppClick = () => {
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
   return (
     <div className="car-card">
       <img src={image} alt={name} className="car-img" />
@@ -9,7 +20,9 @@ export default function CarCard({ name, price, image }) {
       <div className="button-group">
         <button className="call-btn">Call Us Now</button>
         <span className="or-text">OR</span>
-        <button className="whatsapp-btn">WhatsApp Now</button>
+        <button className="whatsapp-btn" onClick={handleWhatsAppClick}>
+          WhatsApp Now
+        </button>
       </div>
     </div>
   );
